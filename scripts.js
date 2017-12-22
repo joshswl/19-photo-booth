d343e734520c70f1405cb7e0edc865689a021000
Josh Lee <joshswl@gmail.com>
2017-12-22 13:12:44 -0600

    takePhoto() - base64

--- scripts.js
+++ scripts.js
@@ -21,6 +21,22 @@ function paintToCanvas() {
     const height = video.videoHeight;
     canvas.width = width;
     canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    // played the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    console.log(data);
 }
 
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
